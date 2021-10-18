0264e194c6f89bed8e425fb1d0e958660326550f
root <root@192.168.0.106>
2021-10-18 16:35:51 +0800

    models,admin的修改

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Category, Tag, Post
+from django.utils.html import format_html
+from django.urls import reverse
 # Register your models here.
 
 
@@ -8,9 +10,48 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'created_time')
     fields = ('name', 'status', 'is_nav')
 
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status',
+                    'created_time', 'operator']
+    list_display_links = []
+    list_filter = ['category', ]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    save_on_top = True
+
+    fields = [
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    ]
+
+    def operator(self, obj):
+        return format_html('<a href="{}">编辑</a>',
+                           reverse('admin:blog_post_change', args=(obj.id, )))
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)
 
--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -19,6 +19,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "分类"
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
@@ -35,6 +38,9 @@ class Tag(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "标签"
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     STATUS_NORMAL = 1
@@ -48,6 +54,7 @@ class Post(models.Model):
     title = models.CharField(max_length=128, verbose_name="标题")
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须为mark down格式")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     category = models.ForeignKey(Category, verbose_name="分类")
     tag = models.ManyToManyField(Tag, verbose_name="标签")
     owner = models.ForeignKey(User, verbose_name="作者")
@@ -56,3 +63,6 @@ class Post(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "文章"
         ordering = ['-id']  #根据Id进行降序排列
+
+    def __str__(self):
+        return self.title
