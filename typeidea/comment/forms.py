ef9492a233dd4188a658e9c1ff33f99ad7d03b38
root <root@192.168.0.106>
2021-10-25 13:46:46 +0800

    调整评论表单样式

--- typeidea/comment/forms.py
+++ typeidea/comment/forms.py
@@ -8,6 +8,18 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    nickname = forms.CharField(max_length=50, label='昵称', widget=forms.widgets.Input(
+        attrs={'class': 'form-control', 'style': 'width: 60%'}
+    ))
+    email = forms.CharField(max_length=50, label='邮箱', widget=forms.widgets.Input(
+        attrs={'class': 'form-control', 'style': 'width: 60%'}
+    ))
+    website = forms.CharField(max_length=50, label='网站', widget=forms.widgets.URLInput(
+        attrs={'class': 'form-control', 'style': 'width: 60%'}
+    ))
+    content = forms.CharField(max_length=50, label='内容', widget=forms.widgets.Textarea(
+        attrs={'class': 'form-control', 'style': 'width: 60%'}
+    ))
 
     def clear_content(self):
         content = self.cleaned_data.get('content')
